c799ec66778091d3f8db39df6ac43f192975d0f6
Vanish <X.KRLUF@GMAIL.COM>
2026-03-18 18:53:04 +0800

    RainRust渲染纹理尺寸 (#172)
    * RainRustDrawObjectsPass 渲染纹理尺寸
    
    * 优化 RainRustVolume 类的属性排列和注释格式

--- Assets/Rendering/RainRust/Volume/RainRustVolume.cs
+++ Assets/Rendering/RainRust/Volume/RainRustVolume.cs
@@ -3,48 +3,52 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-[Serializable]
-[VolumeComponentMenu("Rain Rust/Rain Rust Volume")]
-[SupportedOnRenderPipeline(typeof(UniversalRenderPipelineAsset))]
-public class RainRustVolume : VolumeComponent, IPostProcessComponent
+namespace RainRust.Rendering
 {
-    public BoolParameter isEnabled = new(true);
-    public EnumParameter<RainRustNoiseMode> noiseMode = new(RainRustNoiseMode.Texture);
-    public EnumParameter<RainRustNoiseType> noiseType = new(RainRustNoiseType.Perlin);
-    public EnumParameter<RainRustAlphaMode> alphaMode = new(RainRustAlphaMode.OneAlpha);
-    public TextureParameter noiseTexture = new(null);
-    public Vector4Parameter noiseTilingOffset = new(Vector4.one);
-    public Vector2Parameter noiseVelocity = new(Vector2.zero);
-    public FloatParameter noiseScale = new(10.0f);
-    public ClampedFloatParameter noiseIntensity = new(1.0f, 0f, 10f);
-    public IntParameter lightSamples = new(16);
-    public ClampedFloatParameter lightIntensity = new(0f, 0f, 10f);
-    public ClampedFloatParameter lightFalloffAlpha = new(0.1f, 0.001f, 1f);
-    public ClampedFloatParameter lightFalloffGamma = new(2.0f, 0.001f, 10f);
+    [Serializable]
+    [VolumeComponentMenu("Rain Rust/Rain Rust Volume")]
+    [SupportedOnRenderPipeline(typeof(UniversalRenderPipelineAsset))]
+    public class RainRustVolume : VolumeComponent, IPostProcessComponent
+    {
+        public BoolParameter isEnabled = new(true);
+        public EnumParameter<RainRustNoiseMode> noiseMode = new(RainRustNoiseMode.Texture);
+        public EnumParameter<RainRustNoiseType> noiseType = new(RainRustNoiseType.Perlin);
+        public EnumParameter<RainRustAlphaMode> alphaMode = new(RainRustAlphaMode.OneAlpha);
+        public TextureParameter noiseTexture = new(null);
+        public Vector4Parameter noiseTilingOffset = new(Vector4.one);
+        public Vector2Parameter noiseVelocity = new(Vector2.zero);
+        public FloatParameter noiseScale = new(10.0f);
+        public ClampedFloatParameter noiseIntensity = new(1.0f, 0f, 10f);
+        public ClampedFloatParameter resolutionScalar = new(1.0f, 0.1f, 1.0f);
+        public IntParameter lightSamples = new(16);
+        public ClampedFloatParameter lightIntensity = new(0f, 0f, 10f);
+        public ClampedFloatParameter lightFalloffAlpha = new(0.1f, 0.001f, 1f);
+        public ClampedFloatParameter lightFalloffGamma = new(2.0f, 0.001f, 10f);
 
-    public bool IsActive() => isEnabled.value;
+        public bool IsActive() => isEnabled.value;
 
-    public bool IsTileCompatible() => false;
-}
+        public bool IsTileCompatible() => false;
+    }
 
-public enum RainRustNoiseMode
-{
-    None,
-    Texture,
-    Shader,
-}
+    public enum RainRustNoiseMode
+    {
+        None,
+        Texture,
+        Shader,
+    }
 
-public enum RainRustNoiseType
-{
-    Value,
-    Perlin,
-    Simplex,
-    Voronoi,
-}
+    public enum RainRustNoiseType
+    {
+        Value,
+        Perlin,
+        Simplex,
+        Voronoi,
+    }
 
-public enum RainRustAlphaMode
-{
-    OneAlpha,
-    ObjectsMaskAlpha,
-    NormalizedAlpha,
+    public enum RainRustAlphaMode
+    {
+        OneAlpha,
+        ObjectsMaskAlpha,
+        NormalizedAlpha,
+    }
 }
