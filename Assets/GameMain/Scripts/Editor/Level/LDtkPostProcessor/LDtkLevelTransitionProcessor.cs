3ce69d6048483386988324827f6a2233e02acf6e
vanish <2013107081@qq.com>
2026-03-19 20:27:46 +0800

    update

--- Assets/GameMain/Scripts/Editor/Level/LDtkPostProcessor/LDtkLevelTransitionProcessor.cs
+++ Assets/GameMain/Scripts/Editor/Level/LDtkPostProcessor/LDtkLevelTransitionProcessor.cs
@@ -1,4 +1,5 @@
 using Core;
+using GameMain.RunTime;
 using LDtkUnity;
 using LDtkUnity.Editor;
 using UnityEngine;
@@ -7,38 +8,32 @@ namespace GameMain.Editor
 {
     public class LDtkTransitionProcessor : LDtkPostprocessor
     {
-        protected override void OnPostprocessProject(GameObject root)
-        {
-            Core.Logger.LogInfo($"Post process LDtk project: {root.name}", LogTag.LdtkProcessor);
-            LDtkComponentProject project = root.GetComponent<LDtkComponentProject>();
-            foreach (LDtkComponentWorld world in project.Worlds)
-            {
-                foreach (LDtkComponentLevel level in world.Levels)
-                {
-                    foreach (LDtkComponentLayer layer in level.LayerInstances)
-                    {
-                        LDtkComponentLayerTilesetTiles tiles = layer.GridTiles;
-                        //access tile data!
-
-                        LDtkComponentLayerIntGridValues intGrid = layer.IntGrid;
-                        //access intGrid data!
-
-                        foreach (LDtkComponentEntity entity in layer.EntityInstances)
-                        {
-                            //access entities!
-                        }
-                    }
-                }
-            }
-        }
-
         protected override void OnPostprocessLevel(GameObject root, LdtkJson projectJson)
         {
             Debug.Log($"Post process LDtk level: {root.name}");
             LDtkComponentLevel level = root.GetComponent<LDtkComponentLevel>();
             foreach (LDtkComponentLayer layer in level.LayerInstances)
             {
-                //iterate upon layers
+                if (layer == null || layer.EntityInstances == null)
+                    continue;
+
+                foreach (LDtkComponentEntity entity in layer.EntityInstances)
+                {
+                    if (entity != null && entity.Identifier == LDtkIdentifiers.LevelTransition)
+                    {
+                        var transitionGo = entity.gameObject;
+                        var pos = entity.transform.localPosition;
+                        var size = entity.Size;
+
+                        var collider = transitionGo.AddComponent<BoxCollider2D>();
+                        collider.isTrigger = true;
+
+                        // We assume the pivot is at the top-left corner, thus we need to adjust the position to center the collider
+                        collider.offset = new Vector2(size.x / 2f, -size.y / 2f);
+                        collider.size = size;
+                        transitionGo.AddComponent<LevelTransition>();
+                    }
+                }
             }
         }
     }
