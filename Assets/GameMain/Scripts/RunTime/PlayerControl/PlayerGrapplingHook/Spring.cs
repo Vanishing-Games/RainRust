4582faf91e272d36bae8756232c64cc4d29969fa
vanish <2013107081@qq.com>
2026-04-10 03:59:04 +0800

    update project structure

---
+++ Assets/GameMain/Scripts/RunTime/PlayerControl/PlayerGrapplingHook/Spring.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace GameMain.RunTime
+{
+    public class Spring
+    {
+        public void Update(float deltaTime)
+        {
+            var direction = m_Target - m_Value >= 0 ? 1f : -1f;
+            var force = Mathf.Abs(m_Target - m_Value) * m_Strength;
+            m_Velocity += (force * direction - m_Velocity * m_Damper) * deltaTime;
+            m_Value += m_Velocity * deltaTime;
+        }
+
+        public void Reset()
+        {
+            m_Velocity = 0f;
+            m_Value = 0f;
+        }
+
+        public void SetValue(float value)
+        {
+            m_Value = value;
+        }
+
+        public void SetTarget(float target)
+        {
+            m_Target = target;
+        }
+
+        public void SetDamper(float damper)
+        {
+            m_Damper = damper;
+        }
+
+        public void SetStrength(float strength)
+        {
+            m_Strength = strength;
+        }
+
+        public void SetVelocity(float velocity)
+        {
+            m_Velocity = velocity;
+        }
+
+        public float Value => m_Value;
+
+        private float m_Strength;
+        private float m_Damper;
+        private float m_Target;
+        private float m_Velocity;
+        private float m_Value;
+    }
+}
---
+++ Assets/GameMain/Scripts/RunTime/PlayerControl/PlayerInitialSet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VanishingGames.ECC.Runtime;
+
+namespace GameMain.RunTime
+{
+    public class PlayerInitialSet : PlayerControlCapabilityBase
+    {
+        protected override void SetUpTickSettings()
+        {
+            base.SetUpTickSettings();
+            TickOrderInGroup = (uint)PlayerControlTickOrder.InitialSet;
+        }
+
+        protected override void OnSetup()
+        {
+            base.OnSetup();
+            mPCComponent.FacingDir = 1;
+            StateTag = this;
+            SetStateMachine(PlayerStateMachine.NormalState, EccTag.NormalState);
+            if (mPCComponent.AllBees[0] == null || mPCComponent.AllBees.Count == 0)
+            {
+                var tempbee = GameObject.Find("Bee");
+                if (mPCComponent.AllBees.Count == 0)
+                    mPCComponent.AllBees.Add(tempbee);
+                else
+                    mPCComponent.AllBees[0] = tempbee;
+            }
+        }
+
+        protected override bool OnShouldActivate()
+        {
+            return true;
+        }
+
+        protected override bool OnShouldDeactivate()
+        {
+            return false;
+        }
+
+        protected override void OnTick(float deltaTime) { }
+    }
+}
