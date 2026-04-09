bda6c69957329b4d2350137180bce7dd2d9c5903
vanish <2013107081@qq.com>
2026-04-10 03:53:38 +0800

    gm scripts update

---
+++ Assets/GameMain/Scripts/Editor/Events/GmEditorTestEvents.cs
@@ -0,0 +1,14 @@
+using Core;
+
+namespace GameMain.Editor
+{
+    public class GETE_PlayCoin : IEvent { }
+
+    public class GETE_PlayExplosion : IEvent { }
+
+    public class GETE_PlayJump : IEvent { }
+
+    public class GETE_PlayBgm : IEvent { }
+
+    public class GETE_StopBgm : IEvent { }
+}
