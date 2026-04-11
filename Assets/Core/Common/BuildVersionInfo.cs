43accca7dafe4b03156fb10d93c9fba6904c671c
vanish <2013107081@qq.com>
2026-04-12 04:01:28 +0800

    fmod settings

--- Assets/Core/Common/BuildVersionInfo.cs
+++ Assets/Core/Common/BuildVersionInfo.cs
@@ -5,7 +5,7 @@ namespace Core
 {
     public static class BuildVersionInfo
     {
-        public const string Version = "bc67bf53-dirty";
-        public const string BuildTime = "2026-04-12 02:50:16";
+        public const string Version = "v0.0.1-test-3-g870d5048-dirty";
+        public const string BuildTime = "2026-04-12 04:01:13";
     }
 }
