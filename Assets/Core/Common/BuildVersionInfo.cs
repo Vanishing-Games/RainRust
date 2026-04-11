ea50c715fd7c1eaa638ddefb3a4730a1288c9e89
vanish <2013107081@qq.com>
2026-04-12 06:56:23 +0800

    update

--- Assets/Core/Common/BuildVersionInfo.cs
+++ Assets/Core/Common/BuildVersionInfo.cs
@@ -5,7 +5,7 @@ namespace Core
 {
     public static class BuildVersionInfo
     {
-        public const string Version = "v0.0.1-test6-3-gccccd2b2-dirty";
-        public const string BuildTime = "2026-04-12 06:45:55";
+        public const string Version = "v0.0.1-test6-6-gccabc674-dirty";
+        public const string BuildTime = "2026-04-12 06:55:48";
     }
 }
