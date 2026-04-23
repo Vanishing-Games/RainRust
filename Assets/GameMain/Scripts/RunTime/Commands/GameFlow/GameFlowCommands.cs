53e9deac7d9afda32f45f4dd0a4ad44d0db8d1a9
vanish <2013107081@qq.com>
2026-04-24 02:59:38 +0800

    update

--- Assets/GameMain/Scripts/RunTime/Commands/GameFlow/GameFlowCommands.cs
+++ Assets/GameMain/Scripts/RunTime/Commands/GameFlow/GameFlowCommands.cs
@@ -27,7 +27,7 @@ namespace GameMain.RunTime
 
             public UniTask Execute()
             {
-                GameCore.Instance.RequestLoadLevelFromSavePoint("Chapter1_SavePoint_01");
+                GameCore.Instance.RequestLoadLevelFromSavePoint("Chapter_Platformer_SavePoint_01");
                 return UniTask.CompletedTask;
             }
 
